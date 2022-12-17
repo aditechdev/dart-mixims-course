cbccd5ca0dfbefe5f700b6cfd283a9cb9444817f
Aditya Anand <aditechdevofficial@gmail.com>
2022-12-17 22:13:52 +0530

    :bug: Mixin implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,67 @@
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+mixin canRun {
+  int get speed;
+  void run() {
+    "Running at a speed of $speed".log();
+  }
+}
+
+class Cat with canRun {
+  @override
+  // TODO: implement speed
+  // int get speed => throw UnimplementedError();
+  int speed = 10;
+}
+
+void testIt() {
+  final cat = Cat();
+  cat.run();
+  cat.speed = 20;
+  cat.run();
+}
 
 void main() {
-runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-const MyApp({super.key
-});
+  const MyApp({super.key});
 
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-debugShowCheckedModeBanner: false,
-theme: ThemeData(
-primarySwatch: Colors.blue,
-),
-home: const Home(),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
 }
 
 class Home extends StatelessWidget {
-const Home({super.key
-});
+  const Home({super.key});
 
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-body: Container(),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    testIt();
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "START CODING\n\n</>",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.blue,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
 }
